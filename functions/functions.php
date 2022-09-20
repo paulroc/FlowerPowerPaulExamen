d4ee17cda2075c926e37f84beec6f74823f8d92c
paulroc <54845508+paulroc@users.noreply.github.com>
2022-09-20 09:09:47 +0200

    Ik ga nu ERD proberen aan te leggen.

--- functions/functions.php
+++ functions/functions.php
@@ -1,1 +1,23 @@
 <?php
+$con = mysqli_connect("localhost", "root", "", "FlowerPowerPaul_cms");
+
+if (mysqli_connect_errno()) {
+    echo "The Connection was not established: " . mysqli_connect_error();
+}
+
+
+function getCats(){
+    global $con;
+    $get_cats = "select * from categories";
+
+    $run_cats = mysqli_query($con, $get_cats);
+
+    while ($row_cats = mysqli_fetch_array($run_cats)) {
+        $cat_id = $row_cats['cat_id'];
+
+    }
+
+}
+
+
+
